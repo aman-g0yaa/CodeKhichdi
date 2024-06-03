7a90e3c7bf98dd2879f3f69a9175bb901dc42829
aman-g0yaa <amangoyal2339@gmail.com>
2024-06-04 03:19:23 +0530

    New: Learned query parameter, headers, middleware, body of request

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,32 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 
-app.get('/home',(req,res)=>{
+app.use(bodyParser.json());
+
+function middleware(req, res, next){
+
+    console.log(req.headers);
+    console.log(req.query);
+    console.log(req.body);
+    next();
+
+}               
+
+app.use(middleware); 
+
+
+app.get('/home',(req,res)=>{       
     res.send('Hello World');
-})
+});
 
 app.listen(3000,()=>{
     console.log('Server started at port 3000');
 })
+
+
+
+
+
