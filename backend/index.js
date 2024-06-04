89e27aa9fb055e8768126e9a7a7e2493e8c7c4c4
aman-g0yaa <amangoyal2339@gmail.com>
2024-06-05 04:02:07 +0530

    Learn: Learned how to send data from req to backend, what data does respobnse send, html json simple text, status code 200 range, 400 ranage , 500 ranage

--- backend/index.js
+++ backend/index.js
@@ -18,9 +18,33 @@ function middleware(req, res, next){
 app.use(middleware); 
 
 
-app.get('/home',(req,res)=>{       
-    res.send('Hello World');
-});
+app.get('/home',home);
+
+
+function home(req,res){   
+    if(req.query.apple == "1"){
+
+        var obj = {
+            "apple" : "red" 
+        }
+
+        res.send(`<head>
+        <title>
+            Hello world
+        </title>
+    </head>
+    
+    <body>
+        <b>
+            hi there
+        </b>
+    </body>`);
+    }else{
+        res.status(411).send("gand maara")
+    } 
+    
+}
+
 
 app.listen(3000,()=>{
     console.log('Server started at port 3000');
